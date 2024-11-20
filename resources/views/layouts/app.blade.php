e346de68bd688da215b2a4d539d7132eed37c9af
DorsalFrame <dorsalframe@gmail.com>
2024-11-20 15:18:05 +0200

    roles added to users

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>One Piece Characters</title>
+    <title>One Piece Wiki</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="icon" href="favicon.png" type="image/png">
+    <link rel="icon" href="favicon.ico" type="image/png">
 </head>
         
 @include('layouts.navigation')
