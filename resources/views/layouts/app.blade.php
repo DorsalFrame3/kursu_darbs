e2c7563128dee626677fe301beb6898bce7a3856
DorsalFrame <dorsalframe@gmail.com>
2024-11-26 19:07:14 +0200

    app blade + style fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>One Piece Wiki</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="icon" href="favicon.ico" type="image/png">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>One Piece Wiki</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        <link rel="icon" href="favicon.ico" type="image/png">
+    </head>
         
-@include('layouts.navigation')
+    @include('layouts.navigation')
 
-<!-- Page Content -->
-<main>
-    {{ $slot }}
-</main>
+    <!-- Page Content -->
+    <main>
+        {{ $slot }}
+    </main>
+    @include('layouts.footer')
+</html>
